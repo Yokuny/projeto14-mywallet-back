b0cdbaf2d66ff0bb60503ffdf542fddb81df3f89
yokuny <felipe.vni@hotmail.com>
2023-04-19 15:58:19 -0300

    getHome

--- src/app.js
+++ src/app.js
@@ -86,7 +86,19 @@ app.post("/nova-transacao/:tipo", async (req, res) => {
     return res.status(500).send("Internal server error");
   }
 });
-
+app.get("/home", async (req, res) => {
+  const { token } = req.headers;
+  try {
+    const findingTonken = await db.collection("users").findOne({ token });
+    if (!findingTonken) {
+      return res.status(401).send("Invalid token");
+    }
+    const { transactions } = findingTonken;
+    return res.status(200).send(transactions);
+  } catch (err) {
+    return res.status(500).send("Internal server error");
+  }
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
