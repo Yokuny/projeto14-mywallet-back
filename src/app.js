a561313a0c2ca1b90d3e8a570eee45861c18092c
yokuny <felipe.vni@hotmail.com>
2023-04-19 15:54:01 -0300

    postNewTransations

--- src/app.js
+++ src/app.js
@@ -13,6 +13,12 @@ const cadastradoSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(3).required(),
 });
+const transationSchema = Joi.object({
+  descricao: Joi.string().required(),
+  valor: Joi.number().required(),
+  data: Joi.string().required(),
+  token: Joi.string().required(),
+});
 
 app.post("/cadastro", async (req, res) => {
   const { email, password } = req.body;
@@ -53,8 +59,33 @@ app.post("/login", async (req, res) => {
     return res.status(500).send("Internal server error");
   }
 });
-  
+app.post("/nova-transacao/:tipo", async (req, res) => {
+  const { tipo } = req.params;
+  const { descricao, valor, data, token } = req.body;
 
+  const getError = transationSchema.validate({ descricao, valor, data, token }, { abortEarly: false });
+  if (getError.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+  try {
+    const findingTonken = await db.collection("users").findOne({ token });
+    if (!findingTonken) {
+      return res.status(401).send("Invalid token");
+    }
+    const { _id } = findingTonken;
+    const newTransaction = {
+      descricao,
+      valor,
+      data,
+      tipo,
+    };
+    await db.collection("users").updateOne({ _id }, { $push: { transactions: newTransaction } });
+    return res.status(201).send("Transaction registered successfully");
+  } catch (err) {
+    return res.status(500).send("Internal server error");
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
