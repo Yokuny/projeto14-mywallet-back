9289bafd3d02df5b933f08797614c6fd2d777c18
yokuny <felipe.vni@hotmail.com>
2023-04-19 15:52:20 -0300

    postCadastro

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import Joi from "joi";
+import { v4 as newToken } from "uuid";
 import dbConnection from "./scripts/dbConnection.js";
 
 const app = express();
@@ -8,14 +9,87 @@ app.use(express.json());
 dotenv.config();
 const db = await dbConnection();
 
-app.post("/cadastro", (req, res) => {
-  const { email, password } = req.body;
-  // password no mínimo três caracteres. Caso não status 422 (Unprocessable Entity) mensagem explicando.
-  // email deve estar no formato correto. 422 (Unprocessable Entity) mensagem explicando o erro.
-  // se e-mail cadastrado, retornar status code 409 (Conflict) mensagem explicando o erro.
-  // campos não presente ou inválido, retornar `422 (Unprocessable Entity)`mensagem explicando.
-  // caso de sucesso, a requisição deve retornar status `201 (Created)`.
+const cadastradoSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(3).required(),
 });
+const transationSchema = Joi.object({
+  descricao: Joi.string().required(),
+  valor: Joi.number().required(),
+  data: Joi.string().required(),
+  token: Joi.string().required(),
+});
+
+app.post("/cadastro", async (req, res) => {
+  const { email, password } = req.body;
+  const getError = cadastradoSchema.validate({ email, password }, { abortEarly: false });
+  if (getError.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+  try {
+    const Registered = await db.collection("users").findOne({ email });
+    if (Registered) {
+      return res.status(409).send("Email already registered");
+    }
+    await db.collection("users").insertOne({ email, password });
+    return res.status(201).send("Registered successfully");
+  } catch (err) {
+    return res.status(500).send("Internal server error");
+  }
+});
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const getError = cadastradoSchema.validate({ email, password }, { abortEarly: false });
+  if (getError.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+  try {
+    const record = await db.collection("users").findOne({ email });
+    if (!record) {
+      return res.status(404).send("Email not registered");
+    }
+    if (record.password !== password) {
+      return res.status(401).send("Invalid password");
+    }
+    const token = newToken();
+    return res.status(200).send(token);
+  } catch (err) {
+    return res.status(500).send("Internal server error");
+  }
+});
+
+app.post("/nova-transacao/:tipo", async (req, res) => {
+  const { tipo } = req.params;
+  const { descricao, valor, data, token } = req.body;
+
+  const getError = transationSchema.validate({ descricao, valor, data, token }, { abortEarly: false });
+  if (getError.error) {
+    const errors = validation.error.details.map((detail) => detail.message);
+    return res.status(422).send(errors);
+  }
+  try {
+    const findingTonken = await db.collection("users").findOne({ token });
+    if (!findingTonken) {
+      return res.status(401).send("Invalid token");
+    }
+    const { _id } = findingTonken;
+    const newTransaction = {
+      descricao,
+      valor,
+      data,
+      tipo,
+    };
+    await db.collection("users").updateOne({ _id }, { $push: { transactions: newTransaction } });
+    return res.status(201).send("Transaction registered successfully");
+  } catch (err) {
+    return res.status(500).send("Internal server error");
+  }
+});
+
+app.get("/home", async (req, res) => {
+    
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
