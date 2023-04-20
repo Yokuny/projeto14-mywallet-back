daf404eb04877c814f1b6cc79bdbeda079388e00
yokuny <felipe.vni@hotmail.com>
2023-04-20 11:26:59 -0300

    register working

--- src/routes/postCadastro.js
+++ src/routes/postCadastro.js
@@ -1,20 +1,21 @@
-import cadastradoSchema from "../scripts/shema.js";
+import { cadastradoSchema } from "../scripts/shema.js";
 const postCadastro = async (req, res, db) => {
-  const { email, password } = req.body;
-  const getError = cadastradoSchema.validate({ email, password }, { abortEarly: false });
+  const { name, email, password } = req.body;
+  console.log(req.body);
+  const getError = cadastradoSchema.validate({ email, password, name }, { abortEarly: false });
   if (getError.error) {
-    const errors = validation.error.details.map((detail) => detail.message);
+    const errors = getError.error.details.map((detail) => detail.message);
     return res.status(422).send(errors);
   }
   try {
     const Registered = await db.collection("users").findOne({ email });
     if (Registered) {
-      return res.status(409).send("Email already registered");
+      return res.status(409).send({ message: "Email already registered" });
     }
     await db.collection("users").insertOne({ email, password });
-    return res.status(201).send("Registered successfully");
+    return res.status(201).send({ message: "Registered successfully" });
   } catch (err) {
-    return res.status(500).send("Internal server error");
+    return res.status(500).send({ message: "Internal server error" });
   }
 };
 export default postCadastro;
