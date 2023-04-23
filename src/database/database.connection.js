b493113e339172f2c74f8f923ed96e934d47360a
yokuny <felipe.vni@hotmail.com>
2023-04-23 13:44:18 -0300

    database connection update

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,9 +1,15 @@
-import { MongoClient } from "mongodb";
+import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
 const URI = process.env.DATABASE_URL;
 const dbConnection = async () => {
-  const database = new MongoClient(URI);
+  const database = new MongoClient(URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
   try {
     await database.connect();
     return database.db();
@@ -12,4 +18,3 @@ const dbConnection = async () => {
   }
 };
 export const db = await dbConnection();
-
