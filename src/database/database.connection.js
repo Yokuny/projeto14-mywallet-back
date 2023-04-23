187017d27792336286c99d0fbfecc68c552cbbd0
yokuny <felipe.vni@hotmail.com>
2023-04-23 17:38:18 -0300

    database string

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,10 +1,9 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
-const URI = process.env.DATABASE_URL;
-console.log("database url:", URI);
+console.log("database url:", process.env.DATABASE_URL);
 const dbConnection = async () => {
-  const database = new MongoClient(URI, {
+  const database = new MongoClient(process.env.DATABASE_URL, {
     serverApi: {
       version: ServerApiVersion.v1,
       strict: true,
