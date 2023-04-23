752702b6e8924ce5599e5138976586065505d4bb
yokuny <felipe.vni@hotmail.com>
2023-04-23 17:49:08 -0300

    database fix, now is working on deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,7 +1,6 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
 import dotenv from "dotenv";
 dotenv.config();
-console.log("database url:", process.env.DATABASE_URL);
 const dbConnection = async () => {
   const database = new MongoClient(process.env.DATABASE_URL, {
     serverApi: {
@@ -11,11 +10,9 @@ const dbConnection = async () => {
     },
   });
   try {
-    console.log("try connect");
     await database.connect();
     return database.db();
   } catch (err) {
-    console.log("error connect");
     console.log({ message: err.message });
   }
 };
